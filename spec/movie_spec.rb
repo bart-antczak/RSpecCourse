ad9c1b92aa7c1a56ad448d51528d21e4ce8efb15
bart-mant-io <antczak.bar@gmail.com>
2020-04-27 09:47:54 +0200

    Movie class to mocks, allow method

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,3 +1,4 @@
+# movie_spec.rb
 class Actor
   def initialize(name)
     @name = name
@@ -9,11 +10,11 @@ class Actor
   end
 
   def act
-    'I love you, baby'
+    "I love you, baby"
   end
 
   def fall_off_ladder
-    'Call my agent! No way!'
+    "Call my agent! No way!"
   end
 
   def light_on_fire
@@ -22,7 +23,7 @@ class Actor
 end
 
 class Movie
-  attr_accessor :actor
+  attr_reader :actor
 
   def initialize(actor)
     @actor = actor
@@ -30,13 +31,32 @@ class Movie
 
   def start_shooting
     if actor.ready?
-      puts actor.act
-      puts actor.fall_off_ladder
-      puts actor.light_on_fire
+      actor.act
+      actor.fall_off_ladder
+      actor.light_on_fire
+      actor.act
     end
   end
 end
 
-actor = Actor.new('Brad Pitt')
-movie = Movie.new(actor)
-movie.start_shooting
+# actor = Actor.new('Brad Pitt')
+# movie = Movie.new(actor)
+# movie.start_shooting
+
+RSpec.describe Movie do
+  let(:stuntman) { double("Mr. Danger", ready?: true, act: 'Any string at all', fall_off_ladder: 'Sure! Lets do it', light_on_fire: true)}
+  subject { described_class.new(stuntman) }
+
+  describe '#start_shooting method' do
+    it 'expects an actor to do 3 actions' do
+      # expect(stuntman).to receive(:light_on_fire).once
+      # expect(stuntman).to receive(:light_on_fire).exactly(1).times
+      expect(stuntman).to receive(:light_on_fire).at_most(1).times
+
+      # expect(stuntman).to receive(:act).twice
+      # expect(stuntman).to receive(:act).exactly(2).times
+      expect(stuntman).to receive(:act).at_least(2).times
+      subject.start_shooting
+    end
+  end
+end
